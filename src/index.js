285ae1154578037d1e406e1691e48d314954d85d
Ethan <emekanwankwo201@gmail.com>
2018-10-31 21:41:04 +0100

    Add get distance with traffic method

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ const DistanceMatrix = require('./build/distanceMatrixService');
 
 module.exports = {
   getDistanceMatrix: DistanceMatrix.getDistanceMatrix,
-  getMockDistanceMatrix: DistanceMatrix.getMockDistanceMatrix
+  getMockDistanceMatrix: DistanceMatrix.getMockDistanceMatrix,
+  getDistanceMatrixWithTraffic: DistanceMatrix.getDistanceMatrixWithTraffic
 }
